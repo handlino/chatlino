66e0f00f04c94bbd31e160c6ae8e59bd8fe46cee
Shelling Hsu <navyblueshellingford@gmail.com>
2008-07-01 08:22:14 +0000

    r490@shelling (orig r489):  xdite | 2008-06-27 01:51:14 +0800  done chatrooms list
    git-svn-id: http://code.handlino.com/svn/Chatlino/trunk@551 b19cb30a-9f2e-4084-8847-5e1a13269302

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,7 +3,11 @@ class ChatroomsController < ApplicationController
   before_filter :login_required
   
   include ChatSystem
-  
+
+  def index
+    @chatrooms = Chatroom.find(:all)
+    render :layout => "application"
+  end
   def new
     
   end
