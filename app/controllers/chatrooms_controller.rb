342c7557cfa6e5df6875d7a24f4ec87dd1f1897a
Shelling Hsu <navyblueshellingford@gmail.com>
2008-07-01 08:19:30 +0000

    r483@shelling (orig r482):  ihower | 2008-06-26 17:40:22 +0800  * add empty Chatrooms controller
    git-svn-id: http://code.handlino.com/svn/Chatlino/trunk@544 b19cb30a-9f2e-4084-8847-5e1a13269302

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,21 @@
+class ChatroomsController < ApplicationController
+  
+  before_filter :login_required
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+end
