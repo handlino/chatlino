2a1a8b7d3e8bbe930c721cb9a3a0bd340fc098f5
Shelling Hsu <navyblueshellingford@gmail.com>
2008-07-01 08:22:47 +0000

    r492@shelling (orig r491):  xdite | 2008-06-27 02:38:07 +0800  REST chatlino chatrooms_controller / edit - update - destroy
    git-svn-id: http://code.handlino.com/svn/Chatlino/trunk@553 b19cb30a-9f2e-4084-8847-5e1a13269302

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,9 @@
 class ChatroomsController < ApplicationController
   
   before_filter :login_required
-  
+ 
+  # TODO chatroom permission
+
   include ChatSystem
 
   def index
@@ -10,19 +12,34 @@ class ChatroomsController < ApplicationController
   end
   def new
     @chatroom = Chatroom.new
-
   end
   
   def create
-    
+    @chatroom = Chatroom.new(params[:chatroom])
+    @chatroom.owner = current_user
+    if @chatroom.save
+      redirect_to chatrooms_path
+    else
+      render :action => "new"
+    end
   end
   
   def edit
-    
+    @chatroom = Chatroom.find(params[:id]) 
   end
-  
+
   def update
-    
+    @chatroom = Chatroom.find(params[:id])
+    if @chatroom.update_attributes( params[:chatroom] )
+      redirect_to chatrooms_path
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @chatroom.destroy
+    redirect_to chatrooms_path
   end
   
 end
